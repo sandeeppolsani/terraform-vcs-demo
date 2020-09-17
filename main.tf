9f1b395ba940fea42e2efe3922cf9dfe230c2fb6
Polsani sandeep <polsani.sandeep@gmail.com>
2020-09-17 18:33:49 +0530

    Hey i included 2 instances

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "AKIAXCBFWQIANA6LO5MW"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+data "aws_vpc" "vpc_data" {
+  count = [{ map = "value",danger = "naresh" }]
+  default = true
+}
+
+resource "aws_instance" "one-ec2" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name      = "instance-1"
+    creater   = "sandeep"
+    createdOn = "terraform"
+  }
+}
+resource "aws_instance" "two-ec2" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name      = "instance-2"
+    creater   = "sandeep"
+    createdOn = "terraform"
+  }
+}
+output "vpc" {
+  value = data.aws_vpc.vpc_data
+}
